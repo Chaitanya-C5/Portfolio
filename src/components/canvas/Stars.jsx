5ad213ff7629b7bf92464fec3465c0c6561084d4
Chaitanya-C5 <gellachaitanyavenkatasai@gmail.com>
2024-10-03 17:23:01 +0530

    mobile render

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,52 +1,51 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/no-unknown-property */
-import { Suspense, useRef, useState } from "react";
+import { Suspense, useRef, useState, useEffect } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
-import CanvasLoader from "../Loader";
-import * as random from 'maath/random/dist/maath-random.esm'
+import * as random from 'maath/random/dist/maath-random.esm';
 
 const Stars = (props) => {
   const ref = useRef();
-  const [sphere] = useState(() => random.inSphere(new Float32Array(3000), { radius: 1.2 }));
+  
+  // Using a variable to check if the user is on mobile or not
+  const isMobile = window.matchMedia('(max-width: 500px)').matches;
+  
+  const [sphere] = useState(() => random.inSphere(new Float32Array(isMobile ? 2000 : 6000), { radius: 1.2 }));
 
   useFrame((state, delta) => {
+    if (ref.current) {
       ref.current.rotation.x -= delta / 10;
       ref.current.rotation.y -= delta / 15;
+    }
   });
 
   return (
-      <group rotation={[0, 0, Math.PI / 4]}>
-          <Points
-              ref={ref}
-              positions={sphere}
-              stride={3}
-              frustumCulled
-              {...props}>
-              <PointMaterial
-                  transparent
-                  color="#f272c8"
-                  size={0.002}
-                  sizeAttenuation={true}
-                  depthWrite={false}
-              />
-          </Points>
-      </group>
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+        <PointMaterial
+          transparent
+          color="#f272c8"
+          size={0.002}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
+    </group>
   );
 };
 
 const StarsCanvas = () => {
   return (
-      <div className="w-full h-auto absolute inset-0 z-[-1]">
-          <Canvas camera={{ position: [0, 0, 1] }}>
-              <Suspense fallback={null}>
-                  <Stars />
-              </Suspense>
-
-              <Preload all />
-          </Canvas>
-      </div>
+    <div className="w-full h-auto absolute inset-0 z-[-1]">
+      <Canvas camera={{ position: [0, 0, 1] }}>
+        <Suspense fallback={null}>
+          <Stars />
+        </Suspense>
+        <Preload all />
+      </Canvas>
+    </div>
   );
 };
 
-export default StarsCanvas
+export default StarsCanvas;
