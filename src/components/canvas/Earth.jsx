b22bf74c89bc5a2ee04c5aa38c2da3f255fd85cb
Chaitanya-C5 <gellachaitanyavenkatasai@gmail.com>
2024-10-04 12:49:07 +0530

    earth

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,36 +1,31 @@
-/* eslint-disable no-unused-vars */
 /* eslint-disable react/no-unknown-property */
-import React, { Suspense, useEffect } from "react";
+import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
 import CanvasLoader from "../Loader";
 
 const Earth = () => {
   const earth = useGLTF("./planet/scene.gltf");
+
   return (
-    <primitive object={earth.scene} scale={2.3} position-y={0} rotation-y={0} />
+    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
   );
 };
 
 const EarthCanvas = () => {
-  // Cleanup to prevent memory leaks and context loss
-  useEffect(() => {
-    const renderer = document.querySelector("canvas");
-
-    return () => {
-      if (renderer) {
-        // Dispose of the renderer
-        renderer.remove();
-      }
-    };
-  }, []);
-
   return (
     <Canvas
       shadows
-      frameloop="demand"
+      frameloop='demand'
+      dpr={[1, 2]}
       gl={{ preserveDrawingBuffer: true }}
-      camera={{ position: [-4, 3, 6], fov: 45, near: 0.1, far: 200 }}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6],
+      }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
@@ -40,8 +35,9 @@ const EarthCanvas = () => {
           minPolarAngle={Math.PI / 2}
         />
         <Earth />
+
+        <Preload all />
       </Suspense>
-      <Preload all />
     </Canvas>
   );
 };
