6cde2ce0bd1ea9a7bfc53876401fae63eb30b268
Chaitanya-C5 <gellachaitanyavenkatasai@gmail.com>
2024-10-04 09:26:00 +0530

    computer_canvas

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,18 +1,17 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/no-unknown-property */
 /* eslint-disable no-unused-vars */
-import React, { Suspense, useEffect, useState, useMemo } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
-const Computers = ({isMobile}) => {
-  
-  const computer = useGLTF("./desktop_pc/scene.gltf")
+const Computers = ({ isMobile }) => {
+  const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
     <mesh>
-      <hemisphereLight intensity={4.15} groundColor='black' />
+      <hemisphereLight intensity={4.15} groundColor="black" />
       <pointLight intensity={1} />
       <spotLight
         position={[-20, 50, 10]}
@@ -24,7 +23,7 @@ const Computers = ({isMobile}) => {
       />
       <primitive
         object={computer.scene}
-        scale={ isMobile ? 0.65 : 0.75}
+        scale={isMobile ? 0.65 : 0.75}
         position={isMobile ? [0, -3, -2.2] : [0, -2.8, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
@@ -33,27 +32,49 @@ const Computers = ({isMobile}) => {
 };
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
 
-  const [isMobile, setIsMobile] = useState(false)  
   useEffect(() => {
-    const mediaQuery = window.matchMedia('(max-width: 500px)');
-
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
     setIsMobile(mediaQuery.matches);
 
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
-    }
+    };
 
-    mediaQuery.addEventListener('change',handleMediaQueryChange);
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
 
     return () => {
-      mediaQuery.removeEventListener("change",handleMediaQueryChange)
-    }
-  }, [])
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
+  // WebGL context loss handling
+  useEffect(() => {
+    const handleContextLost = (event) => {
+      event.preventDefault(); // Prevent the default context restoration
+      console.warn("WebGL context lost!");
+      // Optionally, show a message or reload the scene
+    };
+
+    const handleContextRestored = () => {
+      console.info("WebGL context restored.");
+      // Optionally re-initialize or reload the scene
+    };
+
+    const glCanvas = document.querySelector("canvas");
+    glCanvas.addEventListener("webglcontextlost", handleContextLost, false);
+    glCanvas.addEventListener("webglcontextrestored", handleContextRestored, false);
+
+    return () => {
+      glCanvas.removeEventListener("webglcontextlost", handleContextLost);
+      glCanvas.removeEventListener("webglcontextrestored", handleContextRestored);
+    };
+  }, []);
 
   return (
     <Canvas
-      frameloop='demand'
+      frameloop="demand"
       shadows
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
@@ -65,7 +86,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers isMobile={isMobile}/>
+        <Computers isMobile={isMobile} />
       </Suspense>
 
       <Preload all />
